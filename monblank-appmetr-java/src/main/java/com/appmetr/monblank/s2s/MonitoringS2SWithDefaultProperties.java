d9c6b010c66d4478554d8cb58b803fc7f75a3d1e
Tomas Shestakov <shestakovte@gmail.com>
2018-04-03 17:18:33 +0300

    fixed some bugs
    * fixed bugs in monitoring with default properties;
     some methods access has been made protected;
     improved MonitoringStub to do less work

--- monblank-appmetr-java/src/main/java/com/appmetr/monblank/s2s/MonitoringS2SWithDefaultProperties.java
+++ monblank-appmetr-java/src/main/java/com/appmetr/monblank/s2s/MonitoringS2SWithDefaultProperties.java
@@ -6,24 +6,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MonitoringS2SWithDefaultProperties extends MonitoringS2SImpl {
-    private final Map<String, String> defaultProperties;
+    protected final Map<String, String> defaultProperties;
 
     public MonitoringS2SWithDefaultProperties(Map<String, String> defaultProperties) {
         this.defaultProperties = defaultProperties;
     }
 
     @Override protected MonitorKey createMonitorKey(String name, Map<String, String> properties) {
+        if (defaultProperties == null || defaultProperties.isEmpty()) {
+            return super.createMonitorKey(name, properties);
+        }
+
         if (properties == null || properties.isEmpty()) {
             return super.createMonitorKey(name, defaultProperties);
         }
 
         try {
-            properties.putAll(defaultProperties);
+            defaultProperties.forEach(properties::putIfAbsent);
             return super.createMonitorKey(name, properties);
         } catch (UnsupportedOperationException e) { //if properties is singleton or unmodifiable map
-            final HashMap<String, String> newProperties = new HashMap<>(properties);
-            newProperties.putAll(defaultProperties);
-            return super.createMonitorKey(name, new HashMap<>(properties));
+            final HashMap<String, String> newProperties = new HashMap<>(defaultProperties);
+            newProperties.putAll(properties);
+            return super.createMonitorKey(name, newProperties);
         }
     }
 }
