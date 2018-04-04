7ce2985907174ab41fff2ff7ed67f0e36870b309
Tomas Shestakov <t.shestakov@pixonic.com>
2018-04-04 17:54:32 +0300

    MonitorProperties accepts objects instead of strings easy put numbers there

--- monblank-java/src/main/java/com/appmetr/monblank/MonitorProperties.java
+++ monblank-java/src/main/java/com/appmetr/monblank/MonitorProperties.java
@@ -10,20 +10,23 @@ public class MonitorProperties {
         return new MonitorProperties();
     }
 
-    public static MonitorProperties create(String name, String value) {
+    public static MonitorProperties create(String name, Object value) {
         return create().add(name, value);
     }
 
-    public static MonitorProperties create(Map<String, String> propertires) {
+    public static MonitorProperties create(Map<String, String> properties) {
         MonitorProperties monitorProperties = new MonitorProperties();
-        monitorProperties.properties = new HashMap<>(propertires);
-
+        monitorProperties.properties.putAll(properties);
         return monitorProperties;
     }
 
-    public MonitorProperties add(String name, String value) {
-        properties.put(name, value);
+    public MonitorProperties add(String name, Object value) {
+        properties.put(name, String.valueOf(value));
+        return this;
+    }
 
+    public MonitorProperties addAll(Map<String, Object> properties) {
+        properties.forEach(this::add);
         return this;
     }
 
